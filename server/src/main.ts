4f95c95a7fdfd323ceb167c63cd2f7b7606f5ced
NuminousBlue <jdiehl2236@gmail.com>
2022-08-11 00:16:54 -0500

    serve static module

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+  console.log(`Nest App running on ${await app.getUrl()}`);
 }
 
 bootstrap();
