13facfed504a8a150c29f14272482691f473060e
akelstrom <akelstrom@gmail.com>
2022-08-22 18:13:22 -0400

    made updates to connect to database providers

--- server/src/modules/users/users.module.ts
+++ server/src/modules/users/users.module.ts
@@ -3,6 +3,7 @@ import { usersProviders } from './users.providers';
 import { UsersService } from './users.service';
 
 @Module({
-  providers: [UsersService, ...usersProviders]
+  providers: [UsersService, ...usersProviders],
+  exports: [UsersService]
 })
 export class UsersModule {}
